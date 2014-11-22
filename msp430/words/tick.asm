bc5513f1c904ebc473bfd78c43e25c3a81f9ad6e
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-11-22 20:45:52 +0000

    Tick throws an exception
    just like the AVR version does.

--- msp430/words/tick.asm
+++ msp430/words/tick.asm
@@ -1,7 +1,12 @@
 ;C '    -- xt           find word in dictionary
 ;   BL WORD FIND
-;   0= ABORT" ?" ;
+;   0= if -13 throw then ;
     HEADER(XT_TICK,1,27h,DOCOLON)
-        DW XT_BL,XT_WORD,FIND,XT_ZEROEQUAL,XISQUOTE
-        DB 1,'?'
-        DW XT_QABORT,XT_EXIT
+    .DW XT_BL,XT_WORD,FIND,XT_ZEROEQUAL
+    .dw XT_DOCONDBRANCH
+    DEST(PFA_TICK1)
+    .dw XT_DOLITERAL
+    .dw -13
+    .dw XT_THROW
+PFA_TICK1:
+    .dw XT_EXIT
