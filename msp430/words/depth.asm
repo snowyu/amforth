c00be36023a52cba6e280d9b31e233e8585c2412
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-11-22 19:30:43 +0000

    depth depends on the stack pointer words.
    whether a swap or a 1- is used is a matter of taste.

--- msp430/words/depth.asm
+++
@@ -1,4 +0,0 @@
-;C DEPTH    -- +n        number of items on stack
-;   SP@ S0 XT_SWAP - 2/ ;   16-BIT VERSION!
-    HEADER(DEPTH,5,"depth",DOCOLON)
-        DW XT_SP_FETCH,XT_SP0,XT_SWAP,XT_MINUS,TWOSLASH,XT_EXIT
