e03494aae5e6c264771a2697bd0270d4ad947973
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-03 19:13:05 +0000

    Use the new words for .s
    still no common code.

--- msp430/words/dot-s.asm
+++ msp430/words/dot-s.asm
@@ -3,9 +3,10 @@
 ;       SP@ S0 2 - DO I @ U. -2 +LOOP
 ;   THEN ;
     HEADER(XT_DOTS,2,".s",DOCOLON)
-        DW XT_SP_FETCH,XT_SP0,XT_MINUS,qbran
+        DW XT_DEPTH,XT_UDOT,XT_SPACE
+	DW XT_DEPTH, XT_DUP,qbran
         DEST(DOTS2)
-        DW XT_SP_FETCH,XT_SP0,lit,2,XT_MINUS,xdo
-DOTS1:  DW XT_I,XT_FETCH,XT_UDOT,lit,-2,xplusloop
+        DW XT_ZERO,xdo
+DOTS1:  DW XT_I,XT_PICK,XT_UDOT,xloop
         DEST(DOTS1)
 DOTS2:  DW XT_EXIT
