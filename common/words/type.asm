120d65456378e845cbbfc2f01642c50a83362bda
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-05 16:32:22 +0000

    Merged into common code

--- common/words/type.asm
+++ common/words/type.asm
@@ -1,6 +1,12 @@
 ; ( addr n -- ) 
 ; Character IO
 ; print a RAM based string
+
+.if cpu_msp430==1
+    HEADER(XT_TYPE,4,"type",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_TYPE:
     .dw $ff04
     .db "type"
@@ -9,16 +15,18 @@ VE_TYPE:
 XT_TYPE:
     .dw DO_COLON
 PFA_TYPE:
+
+.endif
     .dw XT_BOUNDS
     .dw XT_QDOCHECK
     .dw XT_DOCONDBRANCH
-    .dw PFA_TYPE2
+    DEST(PFA_TYPE2)
     .dw XT_DODO
 PFA_TYPE1:
     .dw XT_I
     .dw XT_CFETCH
     .dw XT_EMIT
     .dw XT_DOLOOP
-    .dw PFA_TYPE1
+    DEST(PFA_TYPE1)
 PFA_TYPE2:
     .dw XT_EXIT
