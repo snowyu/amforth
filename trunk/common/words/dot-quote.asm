170edd7a0de39cbf6d075ca393a81246b2452cbe
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-14 10:54:56 +0000

    Unified ."
    small cleanups.

--- trunk/common/words/dot-quote.asm
+++ trunk/common/words/dot-quote.asm
@@ -1,6 +1,21 @@
 ; ( -- ) (C: "ccc<quote>" -- )
 ; Compiler
 ; compiles string into dictionary to be printed at runtime
+
+.if cpu_msp430==1
+    ; IMMED(DOTQUOTE,2,"."",DOCOLON)
+        DW      link
+        DB      0FEh       ; immediate
+.set link = $
+        DB      2,'.','"'
+        .align 16
+DOTQUOTE: DW      DOCOLON
+
+.endif
+
+.if cpu_avr8==1
+
+
 VE_DOTSTRING:
     .dw $0002
     .db ".",$22
@@ -9,8 +24,8 @@ VE_DOTSTRING:
 XT_DOTSTRING:
     .dw DO_COLON
 PFA_DOTSTRING:
+.endif
     .dw XT_SQUOTE
     .dw XT_COMPILE
     .dw XT_ITYPE
     .dw XT_EXIT
-
