60ee9ec163d1a9fedc7f1c29765e74a18a8994f5
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-14 20:35:21 +0000

    Set assembler label to XT_ notation

--- trunk/msp430/words/j.asm
+++ trunk/msp430/words/j.asm
@@ -1,7 +1,7 @@
 
 ;C J        -- n   R: 4*sys -- 4*sys
 ;C                  get the second loop index
-        CODEHEADER(JJ,1,"j")
+        CODEHEADER(XT_J,1,"j")
         SUB     #2,PSP          ; make room in TOS
         MOV     TOS,0(PSP)
         MOV     @RSP,TOS        ; index = loopctr - fudge
