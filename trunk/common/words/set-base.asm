e4d968be5637b1b016be8139c0576f3ee5b52bae
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-18 19:36:54 +0000

    Table based BASE changing.

--- trunk/common/words/set-base.asm
+++ trunk/common/words/set-base.asm
@@ -1,6 +1,16 @@
 ; ( addr len -- addr' len' ) 
 ; Numeric IO
-; skip a numeric prefix character
+; skip a numeric prefix character and set BASE accordingly
+
+.if cpu_msp430==1
+    HEADLESS(XT_BASES,DOROM)
+.endif
+
+.if cpu_avr8==1
+XT_BASES:
+    .dw PFA_DOCONSTANT
+.endif
+    .dw 10,16,2,10 ; last could be a 8 as well.
 
 .if cpu_msp430==1
     HEADLESS(XT_SET_BASE,DOCOLON)
@@ -11,51 +21,39 @@ XT_SET_BASE:
     .dw DO_COLON 
 PFA_SET_BASE:        ; ( adr1 len1 -- adr2 len2 ) 
 .endif
-    .dw XT_OVER 
-    .dw XT_CFETCH 
-    .dw XT_DUP 
+    .dw XT_OVER
+    .dw XT_CFETCH
     .dw XT_DOLITERAL
-    .dw 36     ; '$' 
-    .dw XT_EQUAL 
-    .dw XT_DOCONDBRANCH
-    DEST(PFA_SETBASE0)
-    .dw XT_HEX
-    .dw XT_DOBRANCH
-    DEST(PFA_SETBASE_FOUND)
-PFA_SETBASE0:
-    .dw XT_DUP 
-    .dw XT_DOLITERAL
-    .dw 37 ; '%' 
-    .dw XT_EQUAL 
-    .dw XT_DOCONDBRANCH
-    DEST(PFA_SETBASE1)
-    .dw XT_BIN
-    .dw XT_DOBRANCH
-    DEST(PFA_SETBASE_FOUND)
-PFA_SETBASE1:
-    .dw XT_DUP 
-    .dw XT_DOLITERAL
-    .dw 38   ; '&'
-    .dw XT_EQUAL 
-    .dw XT_DOCONDBRANCH
-    DEST(PFA_SETBASE2)
-    .dw XT_DECIMAL 
-    .dw XT_DOBRANCH
-    DEST(PFA_SETBASE_FOUND)
-PFA_SETBASE2:
+    .dw 35
+    .dw XT_MINUS
     .dw XT_DUP
+    .dw XT_ZERO
     .dw XT_DOLITERAL
-    .dw 35 ; '#'
-    .dw XT_EQUAL 
+    .dw 4
+    .dw XT_WITHIN
     .dw XT_DOCONDBRANCH
-    DEST(PFA_SETBASE3)
-    .dw XT_DECIMAL 
-PFA_SETBASE_FOUND:
-    .dw XT_DROP
-    .dw XT_DOLITERAL
-    .dw 1
-    .dw XT_SLASHSTRING
-    .dw XT_EXIT
-PFA_SETBASE3:
-    .dw XT_DROP
+    DEST(SET_BASE1)
+	.if cpu_msp430==1
+	    .dw XT_CELLS
+	.endif
+	.dw XT_BASES
+	.dw XT_PLUS
+	.dw XT_FETCHI
+	.dw XT_BASE
+	.dw XT_STORE
+	.dw XT_DOLITERAL
+	.dw 1
+	.dw XT_SLASHSTRING
+	.dw XT_DOBRANCH
+	DEST(SET_BASE2)
+SET_BASE1:
+	.dw XT_DROP
+SET_BASE2:
     .dw XT_EXIT 
+
+; create bases 10 , 16 , 2 , 8 ,
+; : set-base 35 - dup 0 4 within if 
+;    bases + @i base ! 1 /string 
+;   else 
+;    drop
+;   then ;
