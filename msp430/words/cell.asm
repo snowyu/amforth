ee187defd8ad6d156b7aa9e69ba9350e114eaadb
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-21 10:08:38 +0000

    replace cell with 2

--- msp430/words/cell.asm
+++
@@ -1,3 +0,0 @@
-;Z CELL     -- n                 size of one cell
-        HEADER(CELL,4,"cell",DOCON)
-        dw 2
--- msp430/words/variable.asm
+++ msp430/words/variable.asm
@@ -3,4 +3,4 @@
 ; Action of ROMable variable is the same as CREATE; it builds a
 ; constant holding the RAM address.  See CREATE in hilvl430.s43.
         HEADER(VARIABLE,8,"variable",DOCOLON)
-        DW CREATE,CELL,ALLOT,XT_EXIT
+        DW CREATE,XT_TWO,ALLOT,XT_EXIT
