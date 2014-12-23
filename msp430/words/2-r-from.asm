937ca11f6e597f5e5f82d5f33508c77dffc83940
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-23 18:29:19 +0000

    double cell stack transfer working again

--- msp430/words/2-r-from.asm
+++ msp430/words/2-r-from.asm
@@ -1,8 +1,8 @@
 ;C 2R>    -- d    R: d --   pop from return stack
         CODEHEADER(XT_2R_FROM,3,"2r>")
         SUB #4,PSP      ; 2
-        MOV TOS,0(PSP)    ; 4
-        MOV @RSP+,TOS
         MOV TOS,2(PSP)    ; 4
+        MOV @RSP+,TOS
+        MOV TOS,0(PSP)    ; 4
         MOV @RSP+,TOS
         NEXT
