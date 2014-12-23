19d912a24e9635c48f8de4710c17a0652ace4a2f
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-23 18:52:04 +0000

    Forgot the internal SWAPs
     : test 2dup 2>r r@ 2r> ;                                                      
     ok                                                                             
     > 1 2 test . . . . .                                                            
     2 1 2 2 1  ok

--- msp430/words/2-to-r.asm
+++ msp430/words/2-to-r.asm
@@ -1,5 +1,8 @@
 ;C 2>R    d --   R: -- d   push to return stack
         CODEHEADER(XT_2TO_R,3,"2>r")
+        MOV     @PSP,W          ; 2
+        MOV     TOS,0(PSP)      ; 4
+        MOV     W,TOS           ; 1
         PUSH TOS
         MOV @PSP+,TOS
         PUSH TOS
