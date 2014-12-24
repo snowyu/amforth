d1799d9613fed1836b2383c28da247431bf4679a
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-24 10:11:48 +0000

    SAVE keeps INFO_SIZE bytes to INFOSTART, the restore should transfer all these bytes back to RAM. NINIT is correct, but hard to understand

--- msp430/words/cold.asm
+++ msp430/words/cold.asm
@@ -1,18 +1,15 @@
 ;Z COLD     --      cold start Forth system
-;   UINIT U0 #INIT I->D      init user area
-;   ." MSP430 CamelForth etc."
-;   ABORT ;
     HEADER(XT_COLD,4,"cold",DOCOLON)
 	DW XT_BASE,XT_ADDR_TO_INFO,XT_FETCH,XT_DOCONDBRANCH
 	DEST(COLD1)
 	; some valid content in INFO
-        DW XT_DOLITERAL, 1000h
+        DW XT_DOLITERAL, INFOSTART
 	DW XT_DOBRANCH
 	DEST(COLD2)
 COLD1:
         DW UINIT
 COLD2:
-	DW U0,NINIT,ITOD
+	DW U0,XT_DOLITERAL,INFO_SIZE,ITOD
         DW XT_APPLTURNKEY
         DW XT_QUIT
 
