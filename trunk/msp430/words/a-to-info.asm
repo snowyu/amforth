8f595d26bb6c906faf09eed0d8192c1283d1aba1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-12-30 20:56:54 +0000

    Make a magic number speak for itself

--- trunk/msp430/words/a-to-info.asm
+++ trunk/msp430/words/a-to-info.asm
@@ -2,4 +2,4 @@
 ; base a>info @ may be 0 or another value
  HEADER(XT_ADDR_TO_INFO,6,"a>info",DOCOLON)
 
-  .dw XT_DOLITERAL, 1000h, U0,XT_MINUS,XT_PLUS,XT_EXIT
+  .dw XT_DOLITERAL, INFOSTART, U0,XT_MINUS,XT_PLUS,XT_EXIT
