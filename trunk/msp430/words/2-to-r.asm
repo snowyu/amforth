da53e9e18ff26629e0f9851fb1ad20b600a4df40
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-07 19:50:06 +0000

    make the SWAP implicit.

--- trunk/msp430/words/2-to-r.asm
+++ trunk/msp430/words/2-to-r.asm
@@ -1,10 +1,7 @@
 ;C 2>R    d --   R: -- d   push to return stack
         CODEHEADER(XT_2TO_R,3,"2>r")
-        MOV     @PSP,W          ; 2
-        MOV     TOS,0(PSP)      ; 4
-        MOV     W,TOS           ; 1
-        PUSH TOS
-        MOV @PSP+,TOS
+        MOV @PSP+,W
+        PUSH W
         PUSH TOS
         MOV @PSP+,TOS
         NEXT
