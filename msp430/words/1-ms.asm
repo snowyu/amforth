1726d803968308d5c2e9457a1a4c27d21940967b
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-08 11:02:02 +0000

    1ms is too specific to one board. rewritten in Forth and dropped from the assembly words

--- msp430/words/1-ms.asm
+++
@@ -1,9 +0,0 @@
-;  xx 0 DO yy 0 DO LOOP LOOP ;  adjust xx and yy to get a msec.
-     HEADER(XT_1MS,3,"1ms",DOCOLON)
-         DW XT_DOLITERAL,41,XT_ZERO,XT_DODO
-onems1:  DW XT_DOLITERAL,11,XT_ZERO,XT_DODO
-onems2:  DW XT_DOLOOP
-         DEST(onems2)
-         DW XT_DOLOOP
-         DEST(onems1)
-         DW XT_EXIT
