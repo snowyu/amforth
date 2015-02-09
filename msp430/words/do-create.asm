1bd2f328716efbe6c928a72b30d459a42405b66a
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-09 20:54:07 +0000

    starting to redesign wordlist entry creation

---
+++ msp430/words/do-create.asm
@@ -0,0 +1,10 @@
+; create a dictionary entry
+
+HEADER(XT_WLSCOPE,6,"wlsope",DOCOLON)
+    DW XT_GET_CURRENT
+    DW XT_EXIT
+
+HEADER(XT_DOCREATE,8,"(create)",DOCOLON)
+        DW XT_PARSENAME,XT_WLSCOPE  ; ( -- addr len wid)
+	DW XT_HEADER
+	DW XT_EXIT
