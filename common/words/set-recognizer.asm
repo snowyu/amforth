1325bb8c794125a4c26b1c770c88cc642910dc7a
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-12 19:16:06 +0000

    Some labels

--- common/words/set-recognizer.asm
+++ common/words/set-recognizer.asm
@@ -3,18 +3,18 @@
 ; replace the recognizer list
 
 .if cpu_msp430==1
-    HEADER(XT_SET_RECOGNIZER,15,"set-recognizer",DOCOLON)
+    HEADER(XT_SET_RECOGNIZERS,15,"set-recognizers",DOCOLON)
 .endif
 
 .if cpu_avr8==1
-VE_SET_RECOGNIZER:
+VE_SET_RECOGNIZERS:
     .dw $ff0f
     .db "set-recognizers",0
     .dw VE_HEAD
-    .set VE_HEAD = VE_SET_RECOGNIZER
-XT_SET_RECOGNIZER:
+    .set VE_HEAD = VE_SET_RECOGNIZERS
+XT_SET_RECOGNIZERS:
     .dw DO_COLON
-PFA_SET_RECOGNIZER:
+PFA_SET_RECOGNIZERS:
 .endif
     .dw XT_DOLITERAL
     .dw CFG_RECOGNIZERLISTLEN
