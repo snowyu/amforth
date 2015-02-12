f2f511182b9b1848afd9b79a9b283f74a90b33f2
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-12 19:16:58 +0000

    VARIABLE and CONSTANT are common code now

--- common/words/constant.asm
+++ common/words/constant.asm
@@ -1,6 +1,13 @@
 ; ( -- x ) (C: x "<spaces>name" -- )
 ; Compiler
 ; create a constant in the dictionary
+
+.if cpu_msp430==1
+    HEADER(XT_CONSTANT,8,"constant",DOCOLON)
+.endif
+
+.if cpu_avr8==1
+
 VE_CONSTANT:
     .dw $ff08
     .db "constant"
@@ -9,10 +16,10 @@ VE_CONSTANT:
 XT_CONSTANT:
     .dw DO_COLON
 PFA_CONSTANT:
+.endif
     .dw XT_DOCREATE
     .dw XT_REVEAL
     .dw XT_COMPILE
     .dw PFA_DOVARIABLE
     .dw XT_COMMA
     .dw XT_EXIT
-
--- msp430/words/colon.asm
+++ msp430/words/colon.asm
@@ -1,6 +1,6 @@
 ;C :        --      begin a colon definition  
 ;   <BUILDS HIDE ] !COLON ;      Flashable version
     HEADER(XT_COLON,1,":",DOCOLON)
-        DW XT_DOCREATE,XT_HIDE,XT_DOLITERAL,DOCOLON,XT_COMMA
+        DW XT_DOCREATE,XT_DOLITERAL,DOCOLON,XT_COMMA
 	DW XT_RBRACKET
         DW XT_EXIT
