07b94d73bc50ca50c3d022bde071be049cd0c483
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-12 19:16:06 +0000

    Some labels

--- trunk/common/words/get-recognizer.asm
+++ trunk/common/words/get-recognizer.asm
@@ -3,18 +3,18 @@
 ; Get the current recognizer list
 
 .if cpu_msp430==1
-    HEADER(XT_GET_RECOGNIZER,15,"get-recognizer",DOCOLON)
+    HEADER(XT_GET_RECOGNIZERS,15,"get-recognizers",DOCOLON)
 .endif
 
 .if cpu_avr8==1
-VE_GET_RECOGNIZER:
+VE_GET_RECOGNIZERS:
     .dw $ff0f
     .db "get-recognizers",0
     .dw VE_HEAD
-    .set VE_HEAD = VE_GET_RECOGNIZER
-XT_GET_RECOGNIZER:
+    .set VE_HEAD = VE_GET_RECOGNIZERS
+XT_GET_RECOGNIZERS:
     .dw DO_COLON
-PFA_GET_RECOGNIZER:
+PFA_GET_RECOGNIZERS:
 .endif
     .dw XT_DOLITERAL
     .dw CFG_RECOGNIZERLISTLEN
