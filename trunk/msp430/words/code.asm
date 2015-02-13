f25643f34a0d2ee4d3502738b0b1476eae6cf058
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-13 20:04:10 +0000

    smaller code

--- trunk/msp430/words/code.asm
+++ trunk/msp430/words/code.asm
@@ -1,5 +1,5 @@
 ;U MSP430CODE       <name> --       build header for assembler word          
 ;    <builds ihere ihere 2 -  i!  ;
         HEADER(XT_CODE,4,"code",DOCOLON)
-        DW XT_BUILDS,XT_IHERE,XT_DUP,XT_TWO,XT_MINUS,XT_STOREI
+        DW XT_DOCREATE,XT_IHERE,XT_TWO,XT_PLUS,XT_COMMA
         DW XT_REVEAL,XT_EXIT
