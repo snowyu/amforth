de334451a2d1683e0d2e531f2f56ef1b1f7bfb40
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-13 19:10:45 +0000

    :noname and a regression fix for code

--- trunk/common/words/colon.asm
+++ trunk/common/words/colon.asm
@@ -1,6 +1,12 @@
 ; ( -- ) (C: "<spaces>name" -- )
 ; Compiler
 ; create a named entry in the dictionary, XT is DO_COLON
+
+.if cpu_msp430==1
+    HEADER(XT_COLON,1,":",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_COLON:
     .dw $ff01
     .db ":",0
@@ -9,6 +15,7 @@ VE_COLON:
 XT_COLON:
     .dw DO_COLON
 PFA_COLON:
+.endif
     .dw XT_DOCREATE
     .dw XT_COLONNONAME
     .dw XT_DROP
