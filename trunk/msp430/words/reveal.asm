8aa35ab5730359b3dda91af829bee7eba37a988c
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-13 18:00:23 +0000

    Simplify header creation

--- trunk/msp430/words/reveal.asm
+++ trunk/msp430/words/reveal.asm
@@ -1,4 +1,4 @@
 ;Z REVEAL   --      "reveal" latest definition  Flashable
 ;   NEWEST @ LATEST ! ;
     HEADER(XT_REVEAL,6,"reveal",DOCOLON)
-        DW NEWEST,XT_FETCH,XT_LATEST,XT_STORE,XT_EXIT
+        DW XT_NEWEST,XT_FETCH,XT_LATEST,XT_STORE,XT_EXIT
