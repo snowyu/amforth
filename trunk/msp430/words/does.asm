5e793484a295242ad6ed014a33b578156e05aac1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-14 09:17:00 +0000

    Consolidate branch resolving words.

--- trunk/msp430/words/does.asm
+++ trunk/msp430/words/does.asm
@@ -4,4 +4,6 @@
 ; Note that MSP430 uses a JMP, not a CALL, to DODOES.
     IMMED(DOES,5,"does>",DOCOLON)
         DW XT_DOLITERAL,XDOES,XT_COMMA
-        DW XT_DOLITERAL,dodoes,XT_COMMAJMP,XT_EXIT
+        DW XT_DOLITERAL,dodoes
+	DW XT_COMPILE,4030h,XT_COMMA ; compile a machine jump instruction
+	DW XT_EXIT
