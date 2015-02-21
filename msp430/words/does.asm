6de3677c6a5125f4cdcc467f8157a1bb15d6b99f
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-21 18:51:25 +0000

    Fix <builds
    small code optmiziations.

--- msp430/words/does.asm
+++ msp430/words/does.asm
@@ -3,7 +3,7 @@
 ;   dodoes ,JMP ; IMMEDIATE
 ; Note that MSP430 uses a JMP, not a CALL, to DODOES.
     IMMED(DOES,5,"does>",DOCOLON)
-        DW XT_DOLITERAL,XDOES,XT_COMMA
-        DW XT_DOLITERAL,dodoes
-	DW XT_COMPILE,4030h,XT_COMMA ; compile a machine jump instruction
+        DW XT_COMPILE,XT_DODOES
+	DW XT_COMPILE,4030h   ; compile a machine jump instruction
+        DW XT_COMPILE,dodoes
 	DW XT_EXIT
