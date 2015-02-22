c732d2f56188dd03ecdb498ff74caea2741b57b8
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-22 13:22:22 +0000

    Remove <BUILDS from MSP430 assembly sources,   provide it as forth code, common for both.
    MARKER extracted as forth code. 
    
    regressions in USER fixed.

--- trunk/msp430/words/builds.asm
+++
@@ -1,4 +0,0 @@
-;Z <BUILDS  --      define a word with t.b.d. action & no data
-    HEADER(XT_BUILDS,7,"<builds",DOCOLON)
-        DW XT_DOCREATE,XT_REVEAL,XT_MINUSONE,XT_COMMA,XT_EXIT
-
--- trunk/msp430/words/user.asm
+++ trunk/msp430/words/user.asm
@@ -1,7 +1,6 @@
-;Z USER     n --        define user variable 'n'
-;   <BUILDS I, DOES> (machine code fragment)    Flashable model
+;Z USER     n --        define user variable with offset 'n'
         HEADER(XT_USER,4,"user",DOCOLON)
-        DW XT_BUILDS,XT_COMMA,XT_DODOES
+        DW XT_DOCREATE,XT_COMPILE, DOUSER, XT_COMMA, XT_REVEAL,XT_EXIT
 DOUSER: ; -- a-addr     ; add constant to User Pointer, result in TOS
         SUB #2,PSP
         MOV TOS,0(PSP)
@@ -10,6 +9,6 @@ DOUSER: ; -- a-addr     ; add constant to User Pointer, result in TOS
         NEXT
 
 CODEHEADER(XT_UP_FETCH,3,"up@")
-        SUB    #2,PSP
+        SUB #2,PSP
         MOV &UP,TOS
 	NEXT
