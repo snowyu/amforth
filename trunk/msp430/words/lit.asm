4f24af697f39f654234dbe9f908da7ae50ffc7f1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-02-23 19:09:44 +0000

    header less

--- trunk/msp430/words/lit.asm
+++ trunk/msp430/words/lit.asm
@@ -1,6 +1,7 @@
 ;Z lit      -- x    fetch inline literal to stack
 ; This is the primtive compiled by LITERAL.
-        CODEHEADER(XT_DOLITERAL,3,"lit")
+        HEADLESS(XT_DOLITERAL,PFA_DOLITERAL)
+PFA_DOLITERAL:
         SUB #2,PSP      ; 1  push old TOS..
         MOV TOS,0(PSP)  ; 4  ..onto stack
         MOV @IP+,TOS    ; 2  fetch new TOS value
