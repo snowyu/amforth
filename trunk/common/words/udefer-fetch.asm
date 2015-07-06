fa383130cfcb091819b8525ab739c697b6213968
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-07-06 17:30:10 +0000

    clean up defers, improve re-use across platforms

--- trunk/common/words/udefer-fetch.asm
+++ trunk/common/words/udefer-fetch.asm
@@ -1,6 +1,12 @@
 ; ( xt1 -- xt2 ) 
 ; System
 ; does the real defer@ for user based defers
+
+.if cpu_msp430==1
+    HEADER(XT_UDEFERFETCH,7,"Udefer@",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_UDEFERFETCH:
     .dw $ff07
     .db "Udefer@",0
@@ -9,6 +15,7 @@ VE_UDEFERFETCH:
 XT_UDEFERFETCH:
     .dw DO_COLON
 PFA_UDEFERFETCH:
+.endif
     .dw XT_FETCHI
     .dw XT_UP_FETCH
     .dw XT_PLUS
