56ad0fac0898090d353c304b880c1a7785c8f13d
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-07-26 06:47:44 +0000

    common code

--- trunk/common/words/create.asm
+++ trunk/common/words/create.asm
@@ -1,6 +1,12 @@
 ; ( -- a-addr ) (C: "<spaces>name" -- )
 ; Dictionary
 ; create a dictionary header. XT is (constant), with the address of the data field of name
+
+.if cpu_msp430==1
+    HEADER(XT_CREATE,6,"create",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_CREATE:
     .dw $ff06
     .db "create"
@@ -9,6 +15,7 @@ VE_CREATE:
 XT_CREATE:
     .dw DO_COLON
 PFA_CREATE:
+.endif
     .dw XT_DOCREATE
     .dw XT_REVEAL
     .dw XT_COMPILE
