2324a153ebf2eecca2dce6993d61f2862b0a135c
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-11 18:23:54 +0000

    move init to the start address

--- trunk/msp430/amforth.asm
+++ trunk/msp430/amforth.asm
@@ -4,10 +4,13 @@
 .include "itc430.inc"       ; registers, macros, and header structure
 .include "ram.inc"          ; RAM Leyout
 .include "vecs.asm" ; note: sets .org for vector tables
-        .org AMFORTH_START         ; start address of Forth kernel
+
+.org AMFORTH_START         ; start address of Forth kernel
+
+.include "init.asm"
 .include "itc430core.asm"   ; code primitives
 .include "itc430hilvl.asm"
-.include "init.asm"
+
 
 ; now include application specific parts
 .include "dict_appl.inc"
