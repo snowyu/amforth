27edaab1c7a3ab4c15c31c0bf99bff5184d46270
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-18 18:15:37 +0000

    NOOP common code

--- common/words/noop.asm
+++ common/words/noop.asm
@@ -1,12 +1,19 @@
 ; ( -- ) 
 ; Tools
 ; do nothing
+
+.if cpu_msp430==1
+  HEADER(XT_NOOP,4,"noop",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_NOOP:
     .dw $ff04
     .db "noop"
     .dw VE_HEAD
     .set VE_HEAD = VE_NOOP
 XT_NOOP:
-    .dw PFA_NOOP
+    .dw DO_COLON
 PFA_NOOP:
-    jmp_ DO_NEXT
+.endif
+   .DW XT_EXIT
--- msp430/words/noop.asm
+++
@@ -1,3 +0,0 @@
-
-HEADER(XT_NOOP,4,"noop",DOCOLON)
-DW XT_EXIT
