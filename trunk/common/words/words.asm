7a083f7282dc840e1a074ad43b96a3647957b27e
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-23 09:26:11 +0000

    Commonify WORDS

--- trunk/common/words/words.asm
+++ trunk/common/words/words.asm
@@ -1,6 +1,14 @@
 ; ( -- ) 
 ; Tools
 ; prints a list of all (visible) words in the dictionary
+
+.if cpu_msp430==1
+    HEADER(XT_WORDS,5,"words",DOCOLON)
+.endif
+
+.if cpu_avr8==1
+
+
 VE_WORDS:
     .dw $ff05
     .db "words",0
@@ -9,8 +17,9 @@ VE_WORDS:
 XT_WORDS:
     .dw DO_COLON
 PFA_WORDS:
+.endif
     .dw XT_DOLITERAL
-    .dw CFG_ORDERLIST
+    .dw CFG_ORDERLISTLEN+2
     .dw XT_FETCHE
     .dw XT_SHOWWORDLIST
     .dw XT_EXIT
--- trunk/msp430/words/words.asm
+++
@@ -1,4 +0,0 @@
-;X WORDS    --          list all words in dict.
-;   LATEST SHOW-WORDLIST
-    HEADER(XT_WORDS,5,"words",DOCOLON)
-        DW XT_LATEST,XT_SHOWWORDLIST,XT_EXIT
