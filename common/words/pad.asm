069e0969cc859c46499d31655e0c53884c57770d
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-23 17:38:07 +0000

    commonify PAD

--- common/words/pad.asm
+++ common/words/pad.asm
@@ -1,6 +1,12 @@
 ; ( -- a-addr ) 
 ; System Variable
 ; Address of the temporary scratch buffer.
+
+.if cpu_msp430==1
+    HEADER(XT_PAD,3,"pad",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_PAD:
     .dw $ff03
     .db "pad",0
@@ -9,8 +15,9 @@ VE_PAD:
 XT_PAD:
     .dw DO_COLON
 PFA_PAD:
+.endif
     .dw XT_HERE
     .dw XT_DOLITERAL
-    .dw 100
+    .dw 40
     .dw XT_PLUS
     .dw XT_EXIT
