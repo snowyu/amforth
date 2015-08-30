e91e0dfeb7fbc9df8cf2b5c824e3b9658e8e1eaa
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-30 14:37:19 +0000

    I->D is simply a CMOVE, no need to waste space

--- msp430/words/i-to-d.asm
+++
@@ -1,4 +0,0 @@
-;Z I->D     c-addr1 c-addr2 u --  move Code->Data
-; Block move from Code space to Data space.
-; On the MSP430, this is the same as CMOVE.
-       HEADER(XT_ITOD,4,"i->d",XT_CMOVE+2)
