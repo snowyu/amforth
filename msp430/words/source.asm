79fc81e26f2950fe4c430fe7b2e678aab89af48c
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-08-31 17:06:32 +0000

    Make common code

--- msp430/words/source.asm
+++
@@ -1,6 +0,0 @@
-;C SOURCE   -- adr n    current input buffer
-;   'SOURCE 2@ ;        length is at lower adrs
-    DEFER(XT_SOURCE,6,"source")
-        DW USER_SOURCE
-	DW XT_UDEFERFETCH
-	DW XT_UDEFERSTORE
