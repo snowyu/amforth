65208ab70d7f1478706f06e9cd19ba118f78a161
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-09-02 11:32:41 +0000

    Make word creation scope aware

--- trunk/msp430/words/do-create.asm
+++ trunk/msp430/words/do-create.asm
@@ -2,5 +2,6 @@
 
 HEADER(XT_DOCREATE,8,"(create)",DOCOLON)
         DW XT_PARSENAME,XT_WLSCOPE  ; ( -- addr len wid)
-	DW XT_HEADER,XT_NEWEST,XT_STORE
+	DW XT_DUP,XT_NEWEST,XT_CELLPLUS,XT_STORE ; save the wid
+	DW XT_HEADER,XT_NEWEST,XT_STORE          ; save the nt
 	DW XT_EXIT
