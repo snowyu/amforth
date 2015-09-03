00362739c4c6919166bbb4aaab531ec2bd26dde1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-09-03 12:33:48 +0000

    commonify [']

--- common/words/bracketcompile.asm
+++ common/words/bracketcompile.asm
@@ -1,6 +1,12 @@
 ; ( -- c ) (C: "<space>name" -- )
 ; Compiler
 ; Append the compilation semantics of "name" to the dictionary, if any
+
+.if cpu_msp430==1
+    IMMED(XT_BRACKETCOMPILE,9,"[compile]",DOCOLON)
+.endif
+
+.if cpu_avr8==1
 VE_BRACKETCOMPILE:
     .dw $0009
     .db "[compile]",0
@@ -9,6 +15,7 @@ VE_BRACKETCOMPILE:
 XT_BRACKETCOMPILE:
     .dw DO_COLON
 PFA_BRACKETCOMPILE:
+.endif
     .dw XT_COMPILE
     .dw XT_COMPILE
     .dw XT_TICK
