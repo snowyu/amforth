7539345f6319a309e45f699bfdb3252d32dc3cd6
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-09-06 18:46:46 +0000

    Moving environment queries to common code

--- trunk/common/words/env-usersize.asm
+++ trunk/common/words/env-usersize.asm
@@ -1,6 +1,12 @@
 ; ( -- usersize ) 
 ; Environment
 ; size of the USER area in bytes
+.if cpu_msp430==1
+    ENVIRONMENT(XT_ENVUSERSIZE,5,"/user")
+.endif
+
+.if cpu_avr8==1
+
 VE_ENVUSERSIZE:
     .dw $ff05
     .db "/user",0
@@ -9,6 +15,7 @@ VE_ENVUSERSIZE:
 XT_ENVUSERSIZE:
     .dw DO_COLON
 PFA_ENVUSERSIZE:
+.endif
     .dw XT_DOLITERAL
     .dw SYSUSERSIZE + APPUSERSIZE
     .dw XT_EXIT
