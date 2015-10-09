5ac2d296c1293f747b88e73b19810da4cda3f350
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2015-10-09 12:35:45 +0000

    Formatting code

--- trunk/msp430/words/do-does.asm
+++ trunk/msp430/words/do-does.asm
@@ -6,5 +6,11 @@
         DB      7,"(does>)"
         .align 16
 XT_DODOES: DW      DOCOLON
-        DW XT_R_FROM,XT_NEWEST,XT_CELLPLUS,XT_FETCH,XT_FETCH,XT_NFA2CFA,XT_STOREI
+        DW XT_R_FROM
+        DW XT_NEWEST
+        DW XT_CELLPLUS
+        DW XT_FETCH
+        DW XT_FETCH
+        DW XT_NFA2CFA
+        DW XT_STOREI
         DW XT_EXIT
