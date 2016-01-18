13b32ae1b8cbc4a82d232122df02bd544d367cb1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-01-18 19:19:24 +0000

    Fix typo

--- msp430/devices/msp430g2553/words/usart-keyq.asm
+++ msp430/devices/msp430g2553/words/usart-keyq.asm
@@ -1,5 +1,5 @@
 ;X KEY?     -- f    return true if char waiting
-        CODEHEADER(XT_USART_RXQ,3,"tx?")
+        CODEHEADER(XT_USART_RXQ,3,"rx?")
         SUB     #2,PSP          ; 1  push old TOS..
         MOV     TOS,0(PSP)      ; 4  ..onto stack
         BIT.B   #UCA0RXIFG,&IFG2
