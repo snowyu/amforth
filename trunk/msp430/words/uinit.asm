28cbf677a8bb6a117414cfb65729254eafbd3bb2
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-02-21 18:47:48 +0000

    More comments

--- trunk/msp430/words/uinit.asm
+++ trunk/msp430/words/uinit.asm
@@ -5,8 +5,8 @@
 ; variables, followed by 30 cells for interrupt vectors.
     HEADER(XT_UINIT,5,"uinit",DOROM)
 ; CFG Area
-	DW 2,XT_REC_WORD,XT_REC_NUM,0,0
-	DW 1,CFG_FORTHWID,0,0,0,0,0,0,0
+	DW 2,XT_REC_WORD,XT_REC_NUM,0,0 ; content of FORTH-RECOGNIZER
+	DW 1,CFG_FORTHWID,0,0,0,0,0,0,0 ; content of SEARCH ORDER
 	DW XT_APPLTURNKEY ; TURNKEY vector
 	DW RAMDICT        ; HERE / DP
 	DW ROMDICT        ; IHERE / IDP
@@ -14,17 +14,17 @@
 	DW CFG_FORTHWID   ; CURRENT
 	DW lastword       ; FORTH WID
         DW lastenv        ; environment WID
-        DW XT_GET_CURRENT ; wlscope
-	DW CFG_RECOGNIZERLISTLEN ; standard recognizer stack
+        DW XT_GET_CURRENT ; wlscope vector
+	DW CFG_RECOGNIZERLISTLEN ; FORTH-RECOGNIZER
 ; USER Area
-	DW 0,0          ; STATE/FOLLOWER
+	DW 0,0          ; STATE/FOLLOWER (multitasker)
 	DW RSTACK,PSTACK,PSTACK   ; RP0,SP0, SP
-        DW 0            ; HANDLER
+        DW 0            ; HANDLER (Exception stack)
 	DW 10           ; BASE
-	DW XT_USART_TX_POLL  ; EMIT
-	DW XT_USART_TXQ_POLL ; EMIT?
-	DW XT_USART_RX_POLL  ; KEY
-	DW XT_USART_RXQ_POLL ; KEY?
-	DW XT_SOURCETIB
+	DW XT_USART_TX_POLL  ; EMIT vector
+	DW XT_USART_TXQ_POLL ; EMIT? vector
+	DW XT_USART_RX_POLL  ; KEY vector
+	DW XT_USART_RXQ_POLL ; KEY? vector
+	DW XT_SOURCETIB ; SOURCE vector
 	DW 0            ; >IN
-	DW XT_REFILLTIB
+	DW XT_REFILLTIB ; REFILL vector
