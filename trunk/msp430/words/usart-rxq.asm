d08a221a6626c4359a854bbd9177b52370392fce
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-04-17 18:25:53 +0000

    generate well formed flags

--- trunk/msp430/words/usart-rxq.asm
+++ trunk/msp430/words/usart-rxq.asm
@@ -4,9 +4,11 @@ HEADER(XT_USART_RXQ_POLL,3,"rx?",DOCOLON)
   .dw XT_PAUSE
   .dw XT_DOLITERAL
   .dw bm_USART_RXRD
+  .dw XT_DUP
   .dw XT_DOLITERAL
   .dw USART_RX_CFG
   .dw XT_CFETCH
   .dw XT_AND
+  .dw XT_EQUAL
   .dw XT_EXIT
 
