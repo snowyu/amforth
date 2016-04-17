7858b23dd58427474f5163dd68c58d00b34326ea
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-04-17 18:25:53 +0000

    generate well formed flags

--- msp430/words/usart-txq.asm
+++ msp430/words/usart-txq.asm
@@ -5,9 +5,11 @@
 HEADER(XT_USART_TXQ_POLL,3,"tx?",DOCOLON)
   .dw XT_PAUSE
   .dw XT_DOLITERAL
+  .dw bm_USART_TXRD
+  .dw XT_DUP
+  .dw XT_DOLITERAL
   .dw USART_TX_CFG
   .dw XT_CFETCH
-  .dw XT_DOLITERAL
-  .dw bm_USART_TXRD
   .dw XT_AND
+  .dw XT_EQUAL
   .dw XT_EXIT
