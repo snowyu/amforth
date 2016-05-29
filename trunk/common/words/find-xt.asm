834de3b79b374b8176bda45070985e855daf5ce9
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-05-29 14:28:10 +0000

    rename FIND-NAME to find-xt to avoid confusion

--- trunk/common/words/find-xt.asm
+++ trunk/common/words/find-xt.asm
@@ -1,43 +1,43 @@
 ; ( c-addr len --  0 | xt -1 | xt 1 ) 
 ; Tools
-; search wordlists for an entry with the name from c-addr/len
+; search wordlists for an entry with the xt from c-addr/len
 
 .if cpu_msp430==1
-    HEADER(XT_FINDNAME,9,"find-name",DOCOLON)
+    HEADER(XT_FINDXT,7,"find-xt",DOCOLON)
 .endif
 
 .if cpu_avr8==1
-VE_FINDNAME:
-    .dw $ff09
-    .db "find-name",0
+VE_FINDXT:
+    .dw $ff07
+    .db "find-xt",0
     .dw VE_HEAD
-    .set VE_HEAD = VE_FINDNAME
-XT_FINDNAME:
+    .set VE_HEAD = VE_FINDXT
+XT_FINDXT:
     .dw DO_COLON
-PFA_FINDNAME:
+PFA_FINDXT:
 .endif
     .dw XT_DOLITERAL
-    .dw XT_FINDNAMEA
+    .dw XT_FINDXTA
     .dw XT_DOLITERAL
     .dw CFG_ORDERLISTLEN
     .dw XT_MAPSTACK
     .dw XT_ZEROEQUAL
     .dw XT_DOCONDBRANCH
-    DEST(PFA_FINDNAME1)
+    DEST(PFA_FINDXT1)
       .dw XT_2DROP
       .dw XT_ZERO
-PFA_FINDNAME1:
+PFA_FINDXT1:
     .dw XT_EXIT
 
 .if cpu_msp430==1
-    HEADLESS(XT_FINDNAMEA,DOCOLON)
+    HEADLESS(XT_FINDXTA,DOCOLON)
 .endif
 
 .if cpu_avr8==1
 
-XT_FINDNAMEA:
+XT_FINDXTA:
     .dw DO_COLON
-PFA_FINDNAMEA:
+PFA_FINDXTA:
 .endif
     .dw XT_TO_R
     .dw XT_2DUP
@@ -45,11 +45,11 @@ PFA_FINDNAMEA:
     .dw XT_SEARCH_WORDLIST
     .dw XT_DUP
     .dw XT_DOCONDBRANCH
-    DEST(PFA_FINDNAMEA1)
+    DEST(PFA_FINDXTA1)
       .dw XT_TO_R
       .dw XT_NIP
       .dw XT_NIP
       .dw XT_R_FROM
       .dw XT_TRUE
-PFA_FINDNAMEA1:
+PFA_FINDXTA1:
     .dw XT_EXIT
