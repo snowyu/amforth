b2e6243a0a2ae901bb717b3b1584f1cfac225790
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-06-19 14:43:05 +0000

    disable all memory protections. Enable SAVE

--- trunk/msp430/devices/msp430fr5969/init.asm
+++ trunk/msp430/devices/msp430fr5969/init.asm
@@ -14,10 +14,10 @@ reset:
 
 
   mov #0A500h, &MPUCTL0 ; Write password to enable access to MPU
-;  mov #kernelstartadresse>>4, &MPUSEGB1 ; B1 = Forth core start address ($D400)
   mov #FLASHSTART>>4, &MPUSEGB1 ; B1 = Start of memory
   mov #1000h, &MPUSEGB2 ; B2 = 0x10000 (Segment 3 is upper mem)
-  bic #32, &MPUSAM ; Write protect segment 2
+;  bic #32, &MPUSAM ; Write protect segment 2
+  mov #7777h, &MPUSAM ; no protection at all.
   mov #0A501h, &MPUCTL0 ; Enable MPU
   mov.b #0, &MPUCTL0+1 ; Disable MPU access
 
@@ -31,18 +31,18 @@ reset:
 
 
   ;------------------------------------------------------------------------------
-        ; Forth registers
-        MOV     #RSTACK,SP              ; set up stack
-        MOV     #PSTACK,PSP
-        MOV     #UAREA,UP              ; initial user pointer
-        
-	; now hand over to Forth with COLD (a colon word)
-        MOV     #XT_COLD+2,IP
-        NEXT
+  ; Forth registers
+  MOV     #RSTACK,SP              ; set up stack
+  MOV     #PSTACK,PSP
+  MOV     #UAREA,UP              ; initial user pointer
+
+  ; now hand over to Forth with COLD (a colon word)
+  MOV     #XT_COLD+2,IP
+  NEXT
 
 mcu_name:
-	.db 12,"MSP430FR5969"
-	.align 16
+  .db 12,"MSP430FR5969"
+  .align 16
 
 ; ----------------------------------------------------------------------
 ; DEFAULT INTERRUPT HANDLER
