51bcac8e4adb3775484fb7d52008673303eb7758
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-07-04 18:10:27 +0000

    Protect the amforth core code space.

--- trunk/msp430/devices/msp430fr5969/words/cold.asm
+++ trunk/msp430/devices/msp430fr5969/words/cold.asm
@@ -1,21 +1,14 @@
 CODEHEADER(XT_COLD,4,"cold")
 main:   ; Debugger requires the 'main' symbol.
 reset:
-   mov #5A80h, &WDTCTL ; Watchdog off
-
-   bic   #LOCKLPM5, &PM5CTL0  ; Unlock I/O pins
-   mov.b #64, &0225h ; P4DIR  ; Red LED on
-;   mov.b #64, &0223h ; P4OUT
-
+  mov #5A80h, &WDTCTL ; Watchdog off
 
   mov #0A500h, &MPUCTL0 ; Write password to enable access to MPU
-  mov #FLASHSTART>>4, &MPUSEGB1 ; B1 = Start of memory
-  mov #1000h, &MPUSEGB2 ; B2 = 0x10000 (Segment 3 is upper mem)
-;  bic #32, &MPUSAM ; Write protect segment 2
-  mov #7777h, &MPUSAM ; no protection at all.
+  mov FLASHSTART>>4, &MPUSEGB1 ; B1 = Start of memory
+  mov AMFORTH_START>>4, &MPUSEGB2 ; B2 = 0x10000 (Segment 3 is upper mem)
+  mov #7577h, &MPUSAM ; write protect core system.
   mov #0A501h, &MPUCTL0 ; Enable MPU
   mov.b #0, &MPUCTL0+1 ; Disable MPU access
-
 
   ;------------------------------------------------------------------------------
   ; Init Clock
