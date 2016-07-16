b9ea3c67face0353ef0a4b49f9bff0f091d9cad1
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-07-16 18:18:33 +0000

    Change the Prompt words into USER based defers.

--- common/words/prompt-ok.asm
+++ common/words/prompt-ok.asm
@@ -3,7 +3,7 @@
 ; send the READY prompt to the command line
 
 .if cpu_msp430==1
-    HEADLESS(XT_PROMPTOK,DOCOLON)
+    HEADLESS(XT_DEFAULT_PROMPTOK,DOCOLON)
     DW XT_DOSLITERAL
     DB 3," ok" 
 .endif
@@ -14,12 +14,32 @@
 ;    .db "ok"
 ;    .dw VE_HEAD
 ;    .set VE_HEAD = VE_PROMPTOK
-XT_PROMPTOK:
+XT_DEFAULT_PROMPTOK:
     .dw DO_COLON
-PFA_PROMPTOK:
+PFA_DEFAULT_PROMPTOK:
     .dw XT_DOSLITERAL
     .dw 3
     .db " ok",0
 .endif
     .dw XT_ITYPE
     .dw XT_EXIT
+
+; ------------------------
+
+.if cpu_msp430==1
+    DEFER(XT_PROMPTOK,4,"(ok)")
+.endif
+
+.if cpu_avr8==1
+VE_PROMPTOK:
+    .dw $FF04
+    .db "(ok)"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_PROMPTOK
+XT_PROMPTOK:
+    .dw PFA_DODEFER1
+PFA_PROMPTOK:
+.endif
+    .dw USER_P_OK
+    .dw XT_UDEFERFETCH
+    .dw XT_UDEFERSTORE
