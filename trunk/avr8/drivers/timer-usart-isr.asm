553a1e89dcf8cdf094dd298910a800dcf2bee09e
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-07-24 18:39:56 +0000

    Adapt to general ISR framework

--- trunk/avr8/drivers/timer-usart-isr.asm
+++ trunk/avr8/drivers/timer-usart-isr.asm
@@ -1,22 +1,14 @@
 ;;; combined usart/timer interrupt
 
-.set pc_ = pc
-.org TIMER_INT
-  jmp_ usart_timer_isr
-.org pc_
-
 .dseg
 ram_jiffies: .byte 2
 .cseg
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+XT_ISR_TIMER: .dw usart_timer_isr ; it is a code word called from forth vm
 usart_timer_isr:
   push xl
-  in xl, SREG
-  push xl
   push xh
-  push zl
-  push zh
 
   lds zl, low(ram_jiffies)
   lds zh, high(ram_jiffies)
@@ -26,13 +18,9 @@ usart_timer_isr:
   std Z+0, xl
   std Z+1, xh
   call_ usart_rx_buffer
-  pop zh
-  pop zl
   pop xh
   pop xl
-  out SREG, xl
-  pop xl
-  reti
+  jmp_ DO_NEXT
 
 ; ( -- ) Hardware Access
 ; R( --)
@@ -52,6 +40,10 @@ PFA_USART_INIT_RX_ISR:          ; ( -- )
   .dw XT_DOLITERAL
   .dw usart_rx_in
   .dw XT_STORE
+  ; setup the timer isr
+  .dw XT_DOLITERAL,XT_ISR_TIMER
+  .dw XT_DOLITERAL,TIMER_INT
+  .dw XT_INTSTORE
 
   .dw XT_EXIT
 
