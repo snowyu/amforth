73451bfaab735d236d4cbb6f5986e902a88010bd
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2016-07-28 16:51:36 +0000

    Stop saving registers that are not in danger. Thanks to the coordinated ISR handling in the core system.
    Documentation and comments fixed.

--- trunk/avr8/drivers/usart-isr-rx.asm
+++ trunk/avr8/drivers/usart-isr-rx.asm
@@ -2,20 +2,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; forth code:
-; : rx-isr USART_DATA c@
-;    usart_rx_data usart_rx_in c@ dup >r
-;    + !
-;    r> 1+ usart_rx_mask and usart_rx_in c!
-; ;
+; : rx-isr usart-rx-buffer ;
+;
 ; setup with
 ; ' rx-isr URXCaddr int!
 
 XT_ISR_RX:
     .dw usart_rx_isr
 usart_rx_isr:
-  push xl
-  push xh
   call_ usart_rx_buffer
-  pop xh
-  pop xl
   jmp_ DO_NEXT
