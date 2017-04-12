691c1db49fdab7548cd562cbe23437d273c2c61b
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2017-04-12 17:24:22 +0000

    use the cpu register

--- trunk/avr8/words/int-trap.asm
+++ trunk/avr8/words/int-trap.asm
@@ -9,7 +9,6 @@ VE_INTTRAP:
 XT_INTTRAP:
     .dw PFA_INTTRAP
 PFA_INTTRAP:
-    sts intcur, tosl
+    mov isrflag, tosl
     loadtos
-    set ; set the interrupt flag for the inner interpreter
     jmp_ DO_NEXT
