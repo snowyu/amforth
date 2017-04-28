6fb6d94bc50d6fccd90e3e6548c43a5f716da082
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2017-04-28 17:09:12 +0000

    Less code space

--- avr8/words/isr-exec.asm
+++ avr8/words/isr-exec.asm
@@ -9,10 +9,7 @@
 XT_ISREXEC:
     .dw DO_COLON
 PFA_ISREXEC:
-    .dw XT_DOLITERAL
-    .dw intvec
-    .dw XT_PLUS
-    .dw XT_FETCHE
+    .dw XT_INTFETCH
     .dw XT_EXECUTE
     .dw XT_ISREND
     .dw XT_EXIT
