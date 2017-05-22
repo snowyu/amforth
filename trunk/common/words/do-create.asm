f99a40bd76179d9fe01fbc7f245fd35b5de8a04b
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2017-05-22 17:19:42 +0000

    Factor (create-in) to create a named entry in a given wordlist. very lowlevel

--- trunk/common/words/do-create.asm
+++ trunk/common/words/do-create.asm
@@ -17,6 +17,24 @@ XT_DOCREATE:
 PFA_DOCREATE:
 .endif
     .DW XT_PARSENAME,XT_WLSCOPE  ; ( -- addr len wid)
+    .DW XT_DOCREATE_IN           ; ( addr len wid -- )
+    .DW XT_EXIT
+
+
+.if cpu_msp430==1
+   HEADER(XT_DOCREATE_IN,11,"(create-in)",DOCOLON)
+.endif
+
+.if cpu_avr8==1
+VE_DOCREATE_IN:
+    .dw $ff0b
+    .db "(create-in)"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_DOCREATE_IN
+XT_DOCREATE_IN:
+    .dw DO_COLON
+PFA_DOCREATE_IN:
+.endif
     .DW XT_DUP,XT_NEWEST,XT_CELLPLUS,XT_STORE ; save the wid
     .DW XT_HEADER,XT_NEWEST,XT_STORE          ; save the nt
     .DW XT_EXIT
