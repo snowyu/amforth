099c6c2627a225ce2d594dbd0b5e4c6f091c3195
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-17 19:54:50 +0000

    fix 0branch and add more words

--- risc-v/words/warm.s
+++ risc-v/words/warm.s
@@ -3,21 +3,9 @@ COLON Flag_visible, "warm", WARM
   .word XT_LED_INIT
   .word XT_INIT_USART
 PFA_WARM_LOOP:
-  .word XT_RED
-  .word XT_DOLITERAL, 50
-  .word XT_MS
-  .word XT_BLUE
   .word XT_DOLITERAL, 42
   .word XT_SERIAL_EMIT
-  .word XT_DOLITERAL, 50
-  .word XT_MS
-  .word XT_GREEN
-  .word XT_DOLITERAL, 50
-  .word XT_MS
-  .word XT_DOLITERAL
-  .word XT_CR
-  .word XT_EXECUTE
+  .word XT_DOLITERAL,3,XT_SPACES
   .word XT_DOBRANCH
   .word PFA_WARM_LOOP
-  .word XT_YELLOW
   .word XT_EXIT
