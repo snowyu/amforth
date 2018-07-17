6ac200c7246b26c6b05a19b7aa1989eb66260c89
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-17 13:06:41 +0000

    fix 0branch and ms

--- trunk/risc-v/words/ms.s
+++ trunk/risc-v/words/ms.s
@@ -2,14 +2,15 @@
 COLON Flag_visible, "ms", MS
 
 XT_MS_LOOP:
-.word XT_1MINUS
-.word XT_DUP
-.word XT_DOLITERAL
-.word 43
-.word XT_PLUS
-.word XT_EMIT
-.word XT_1MS
-.word XT_DOCONDBRANCH
-.word XT_MS_LOOP
+  .word XT_1MINUS
+  .word XT_DUP
+  .word XT_DOLITERAL
+  .word 48
+  .word XT_PLUS
+  .word XT_EMIT
+  .word XT_1MS
+  .word XT_DUP
+  .word XT_DOCONDBRANCH
+  .word XT_MS_LOOP
 .word XT_DROP
 .word XT_EXIT
