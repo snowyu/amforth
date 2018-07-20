f7ecf38ff31a16b2fa4d6bc2f2f7e51366b843b7
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-20 17:08:47 +0000

    Add very basic numeric output, remove unused assembly words to minimize core size

--- risc-v/amforth.s
+++ risc-v/amforth.s
@@ -3,21 +3,12 @@
 
 .include "preamble.inc"
 
-DOCOLON: 
-        push x16   # IP
-        mv x16,x17  # W->IP
-DO_NEXT:
-        lw x17, 0(x16) # @IP -> W 
-        addi x16,x16,4 # INC IP
-DO_EXECUTE:
-        lw x18, 0(x17) # @W, address of some executable code
-        addi x17,x17,4 # INC W, points now to PFA
-        jalr zero,x18,0 # jump to code
-
 .include "dict_prims.inc"
 .include "dict_secs.inc"
 .include "dict_env.inc"
 .include "dict_drivers.inc"
+
+.equ HERESTART, rampointer
 
 Definition_EndOfCore Flag_invisible, "--- Flash Dictionary ---"
 
---
+++ risc-v/words/dot.s
@@ -0,0 +1,5 @@
+
+COLON Flag_visible, ".", DOT
+   .word XT_S2D, XT_L_SHARP
+   .word XT_SHARP_S, XT_SHARP_GREATER, XT_TYPE
+.word XT_EXIT
