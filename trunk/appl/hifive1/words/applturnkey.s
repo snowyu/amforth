cd5b8230211d9d58422b61752a960be3072a690a
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-21 10:19:47 +0000

    Add catch/throw, make the interpreter prompt look like working and introduce simplified appl-turnkey

---
+++ trunk/appl/hifive1/words/applturnkey.s
@@ -0,0 +1,9 @@
+
+COLON Flag_visible, "turnkey", TURNKEY
+  .word XT_DECIMAL
+  .word XT_LED_INIT
+  .word XT_RED
+  .word XT_INIT_USART
+
+  .word XT_DOT_VER,XT_CR,XT_BUILD_INFO,XT_TYPE
+.word XT_EXIT
--- trunk/risc-v/words/warm.s
+++ trunk/risc-v/words/warm.s
@@ -3,9 +3,6 @@ COLON Flag_visible, "warm", WARM
   .word XT_INIT_RAM
   .word XT_ZERO,XT_TO_IN,XT_STORE
   .word XT_LBRACKET
-  .word XT_DECIMAL
-  .word XT_LED_INIT
-  .word XT_RED
-  .word XT_INIT_USART
+  .word XT_TURNKEY
   .word XT_QUIT
   .word XT_EXIT
