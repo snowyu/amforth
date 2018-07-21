b90e110122be3a54148b14c4e6b5f3d73b808932
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-21 19:10:25 +0000

    Work on wordlist searches

--- trunk/risc-v/words/find-xt.s
+++ trunk/risc-v/words/find-xt.s
@@ -1,4 +1,16 @@
 
 COLON Flag_visible, "find-xt", FINDXT
 
-.word XT_EXIT
+    .word XT_TO_R
+    .word XT_2DUP
+    .word XT_R_FROM
+    .word XT_SEARCH_WORDLIST
+    .word XT_DUP
+    .word XT_DOCONDBRANCH,PFA_FINDXTA1
+      .word XT_TO_R
+      .word XT_NIP
+      .word XT_NIP
+      .word XT_R_FROM
+      .word XT_TRUE
+PFA_FINDXTA1:
+    .word XT_EXIT
