0844fe2a4746577d8928c641727d210e295e25df
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-24 10:24:04 +0000

    fix dubios label names

--- risc-v/words/do-loop.s
+++ risc-v/words/do-loop.s
@@ -1,17 +1,17 @@
 
 HEADLESS DOLOOP
   li x5, 1
-  j DOPLUSLOOP
+  j PFA_DOPLUSLOOP1
 
 HEADLESS DOPLUSLOOP
   mv x5, x3
   loadtos
-DOPLUSLOOP:
+PFA_DOPLUSLOOP1:
   add x8,x8,x5
-  beq  x8,x9,DOLOOP_LEAVE
+  beq  x8,x9,PFA_DOLOOP_LEAVE
   lw x16,0(x16)
   NEXT
-DOLOOP_LEAVE:
+PFA_DOLOOP_LEAVE:
   # restore loop-sys
   lw x10, 0(sp)
   lw x9, 4(sp)
