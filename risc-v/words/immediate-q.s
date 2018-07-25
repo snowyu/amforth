e8816ac6e488e380c743ea2d59a4ec8c118adf70
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-07-25 11:55:35 +0000

    fix immediate flag calculation

--- risc-v/words/immediate-q.s
+++ risc-v/words/immediate-q.s
@@ -1,9 +1,9 @@
 
 NONAME IMMEDIATEQ
     .word  XT_DOLITERAL
-    .word  0x0010
+    .word  Flag_immediate
     .word  XT_AND
-    .word  XT_ZEROEQUAL
+    .word  XT_NOTZEROEQUAL
     .word  XT_DOCONDBRANCH,IMMEDIATEQ1
      .word  XT_DOLITERAL,1
      .word  XT_EXIT
