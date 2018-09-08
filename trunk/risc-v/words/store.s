c74fa288cd4587a65c7dd73c6ba2d251e1b6c77d
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-09-08 16:23:35 +0000

    remove comments

--- trunk/risc-v/words/store.s
+++ trunk/risc-v/words/store.s
@@ -1,8 +1,4 @@
-# -----------------------------------------------------------------------------
   CODEWORD Flag_visible, "!", STORE # ( x 32-addr -- )
-# Given a value 'x' and a cell-aligned address 'addr', stores 'x' to memory at 'addr', consuming both.
-# -----------------------------------------------------------------------------
-store:
   lw x5, 0(x4)
   sw x5, 0(x3)
   lw x3, 4(x4)
