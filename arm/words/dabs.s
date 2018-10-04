2a7a2d3fbc070af9ea137d0e4919c7b6913c4417
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-10-04 13:08:48 +0000

    fix DABS

--- arm/words/dabs.s
+++
@@ -1,6 +0,0 @@
-@------------------------------------------------------------------------------
-  CODEWORD Flag_visible, "dabs", DABS
-@------------------------------------------------------------------------------
-  cmp tos, #0   @ Check sign in high-part
-  bmi dnegate @ Not negative ? Nothing to do !
-NEXT
--- risc-v/words/dabs.s
+++
@@ -1,6 +0,0 @@
-#------------------------------------------------------------------------------
-  CODEWORD Flag_visible, "dabs", DABS
-#------------------------------------------------------------------------------
-dabs:
-  blt x3, zero, PFA_DNEGATE # Check sign in high-part
-  NEXT                  # Not negative ? Nothing to do !
