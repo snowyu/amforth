0c034a5712edae6e1f17728196860c1e119c24aa
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-10-07 10:08:47 +0000

    always wait for cts

--- trunk/risc-v/words/usart-tx-poll.s
+++ trunk/risc-v/words/usart-tx-poll.s
@@ -3,6 +3,11 @@
   CODEWORD Flag_visible, "serial-emit", SERIAL_EMIT
 # -----------------------------------------------------------------------------
 
+SERIAL_EMIT_WAIT:
+  li x5, UART0_TXDATA
+  lw x5, 0(x5)
+  blt x5,zero, SERIAL_EMIT_WAIT
+
   li x6, UART0_TXDATA
   sw x3, 0(x6)
   loadtos
