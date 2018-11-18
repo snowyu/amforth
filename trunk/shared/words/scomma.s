05e00519db6cde606e1f2fa399e1cac68034e196
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-11-18 10:55:40 +0000

    S, working with full address range.

--- trunk/shared/words/scomma.s
+++ trunk/shared/words/scomma.s
@@ -1,17 +1,7 @@
 
 COLON Flag_visible, "s,", SCOMMA
-    .word XT_DUP
-    .word XT_DOSCOMMA
-    .word XT_EXIT
-
-NONAME DOSCOMMA
-    .word XT_COMMA
-    .word XT_DUP 
-    .word XT_2SLASH
-    .word XT_TUCK  
-    .word XT_2STAR 
-    .word XT_MINUS 
-    .word XT_TO_R
+    .word XT_DUP, XT_COMMA
+    .word XT_ALIGNED, XT_2SLASH,XT_2SLASH
     .word XT_ZERO
     .word XT_QDOCHECK
     .word XT_DOCONDBRANCH, PFA_SCOMMA2
@@ -25,12 +15,5 @@ PFA_SCOMMA1:
     .word XT_DOLOOP
     .word PFA_SCOMMA1
 PFA_SCOMMA2:
-    .word XT_R_FROM
-    .word XT_GREATERZERO
-    .word XT_DOCONDBRANCH, PFA_SCOMMA3
-      .word XT_DUP
-      .word XT_CFETCH
-      .word XT_COMMA
-PFA_SCOMMA3:
     .word XT_DROP 
     .word XT_EXIT
