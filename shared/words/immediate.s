611cef2ead21c606b379e62f36dec86328253f2d
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-12-09 19:38:48 +0000

    add IMMEDIATE. tested with quotations.

---
+++ shared/words/immediate.s
@@ -0,0 +1,7 @@
+
+COLON Flag_visible, "immediate", IMMEDIATE
+
+    .word XT_GET_CURRENT,XT_EXECUTE
+    .word XT_DUP, XT_DOLITERAL,Flag_immediate
+    .word XT_ROT, XT_FETCH, XT_OR, XT_SWAP, XT_STORE
+    .word XT_EXIT
