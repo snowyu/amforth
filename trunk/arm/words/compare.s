b1448a989d3702722f2884e6ebdb1afd71cfbe00
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-12-10 16:46:31 +0000

    enable the WANT_IGNORECASE option at build time.

--- trunk/arm/words/compare.s
+++ trunk/arm/words/compare.s
@@ -5,6 +5,20 @@
   mvns tos,tos
 NEXT
 
+.if WANT_IGNORECASE==1
+.macro lowercase Register @ Ein Zeichen in einem Register wird auf Lowercase umgestellt.
+  @    Hex Dec  Hex Dec
+  @ A  41  65   61  97  a
+  @ Z  5A  90   7A  122 z
+  cmp \Register, #0x41
+  blo 5f
+  cmp \Register, #0x5B
+  it lo
+  addlo \Register, #0x20
+5:  
+.endm
+.endif
+
 compare:
   push {lr}
 
@@ -28,6 +42,11 @@ compare:
      subs r0, #1
      ldrb r2, [r1, r0]
      ldrb r3, [tos, r0]
+
+.if WANT_IGNORECASE==1
+     lowercase r2
+     lowercase r3
+.endif
 
      cmp r2, r3
      beq 2b
--- trunk/risc-v/words/compare.s
+++ trunk/risc-v/words/compare.s
@@ -2,6 +2,21 @@
   CODEWORD Flag_visible, "compare",COMPARE
 # -----------------------------------------------------------------------------
 
+.if WANT_IGNORECASE==1
+.macro lowercase Register # Convert to lowercase.
+  #    Hex Dec  Hex Dec
+  # A  41  65   61  97  a
+  # Z  5A  90   7A  122 z
+  
+  sltiu x14, \Register, 0x41
+  bne x14, zero, 5f
+    sltiu x14, \Register, 0x5B
+    beq x14, zero, 5f
+      addi \Register, \Register, 0x20    
+5:
+.endm
+.endif
+
   mv x10, x3
   lw x11, 0(x4)
   lw x12, 4(x4)
@@ -18,9 +33,13 @@
 2:beq x10, zero, 3f    # Any characters to compare left ?
 
   lbu x12, 0(x11)
-#  lowercase x12
+.if WANT_IGNORECASE==1
+  lowercase x12
+.endif
   lbu x13, 0(x3)
-#  lowercase x13
+.if WANT_IGNORECASE==1
+  lowercase x13
+.endif
   bne x12, x13, 1b
   
   addi x10, x10, -1
