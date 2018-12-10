11cadfb261e4f6da155d18714767ea085d5f678c
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2018-12-10 13:17:56 +0000

    distinguish between Faults (F) and 0-Handler interrupts, save registers in use

--- arm/common/isr.s
+++ arm/common/isr.s
@@ -1,28 +1,25 @@
 nullhandler:
    push {lr}
+   push {r1}
+   ldr r1, =#48
+   SEMIT r1
    mrs r1, ipsr
-
-   ldr r1, =#48 @ 0
+   add r1, #48 @ +"0"
    SEMIT r1
 
-   adds r1, #48 @ "0"
-   SEMIT r1
-
-
+   pop {r1}
    pop {pc}
 
 faulthandler:
    push {lr}
-
-   mrs r1, ipsr
+   push {r1}
 
    ldr r1, =#70 @ F
    SEMIT r1
-
-   adds r1, #48 @ "0"
+   mrs r1, ipsr
+   adds r1, #48 @ +"0"
    SEMIT r1
-
-
+   pop {r1}
    pop {pc}
 
 .ltorg
